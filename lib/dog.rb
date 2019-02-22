18d761ee8de1dfefcb345f4c3a300477421118a4
Kelsey Low <kklowlow@gmail.com>
2019-02-22 00:16:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
-  def name=(dog_name)
+  def name=(dog_name) # name equals, setter method
     this_dogs_name = dog_name
   end
   
-  def name
+  def name # getter method, reads the name
     this_dogs_name
   end
 end
